e905669dbaf8480dfaa66c3c12eabc37aeef4da2
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-19 00:27:00 +0300

    refactor JT808HeartbeatMessageDecoder

--- src/main/java/by/zuevvlad/jt808/decoder/JT808HeartbeatMessageDecoder.java
+++ src/main/java/by/zuevvlad/jt808/decoder/JT808HeartbeatMessageDecoder.java
@@ -5,6 +5,8 @@ import by.zuevvlad.jt808.model.JT808Message.BodyProperties;
 import io.netty.buffer.ByteBuf;
 import org.springframework.stereotype.Component;
 
+import static by.zuevvlad.jt808.model.JT808HeartbeatMessage.MESSAGE_ID;
+
 @Component
 public final class JT808HeartbeatMessageDecoder extends JT808MessageDecoder<Void, JT808HeartbeatMessage> {
 
@@ -19,12 +21,11 @@ public final class JT808HeartbeatMessageDecoder extends JT808MessageDecoder<Void
     }
 
     @Override
-    protected JT808HeartbeatMessage createMessage(short messageId,
-                                                  BodyProperties bodyProperties,
+    protected JT808HeartbeatMessage createMessage(BodyProperties bodyProperties,
                                                   String phoneNumber,
                                                   short serialNumber,
                                                   Void body,
                                                   byte checkCode) {
-        return new JT808HeartbeatMessage(messageId, bodyProperties, phoneNumber, serialNumber, body, checkCode);
+        return new JT808HeartbeatMessage(bodyProperties, phoneNumber, serialNumber, body, checkCode);
     }
 }
