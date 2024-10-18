6d731ed36a7f4be7fa1217db7693329af974019d
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-18 22:45:19 +0300

    refactor JT808MessageDecodingIT

--- src/test/java/by/zuevvlad/jt808/it/decoding/JT808MessageDecodingIT.java
+++ src/test/java/by/zuevvlad/jt808/it/decoding/JT808MessageDecodingIT.java
@@ -2,12 +2,17 @@ package by.zuevvlad.jt808.it.decoding;
 
 import by.zuevvlad.jt808.base.AbstractSpringBootTest;
 import by.zuevvlad.jt808.decoder.JT808MessagesDecoder;
+import by.zuevvlad.jt808.model.JT808AuthenticationMessage;
 import by.zuevvlad.jt808.model.JT808Message;
 import by.zuevvlad.jt808.model.JT808Message.BodyProperties;
 import by.zuevvlad.jt808.model.JT808TerminalRegistrationMessage;
 import io.netty.buffer.ByteBuf;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.stream.Stream;
 
 import static io.netty.buffer.ByteBufUtil.decodeHexDump;
 import static io.netty.buffer.Unpooled.wrappedBuffer;
@@ -18,27 +23,47 @@ public final class JT808MessageDecodingIT extends AbstractSpringBootTest {
     @Autowired
     private JT808MessagesDecoder decoder;
 
-    @Test
-    public void terminalRegistrationMessageShouldBeDecoded() {
-        ByteBuf givenBuffer = wrappedBuffer(decodeHexDump("7e0100003607006195286504fa0000000038363937374e5438303800000000000000000000000000000031393532383635004c42313233343536373839303132333435607e"));
+    @ParameterizedTest
+    @MethodSource("provideHexDumpAndExpectedMessage")
+    public void messageShouldBeDecoded(String givenHexDump, JT808Message<?> expected) {
+        ByteBuf givenBuffer = wrappedBuffer(decodeHexDump(givenHexDump));
 
         JT808Message<?> actual = decoder.decode(givenBuffer);
-        JT808Message<?> expected = new JT808TerminalRegistrationMessage(
-                (short) 256,
-                new BodyProperties(false, false, 0, 54),
-                "070061952865",
-                (short) 1274,
-                new JT808TerminalRegistrationMessage.Body(
-                        (short) 0,
-                        (short) 0,
-                        "86977",
-                        "NT808",
-                        "1952865",
-                        (byte) 0,
-                        "LB123456789012345"
-                ),
-                (byte) 96
-        );
         assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> provideHexDumpAndExpectedMessage() {
+        return Stream.of(
+                Arguments.of(
+                        "7e0100003607006195286504fa0000000038363937374e5438303800000000000000000000000000000031393532383635004c42313233343536373839303132333435607e",
+                        new JT808TerminalRegistrationMessage(
+                                (short) 256,
+                                new BodyProperties(false, false, 0, 54),
+                                "070061952865",
+                                (short) 1274,
+                                new JT808TerminalRegistrationMessage.Body(
+                                        (short) 0,
+                                        (short) 0,
+                                        "86977",
+                                        "NT808",
+                                        "1952865",
+                                        (byte) 0,
+                                        "LB123456789012345"
+                                ),
+                                (byte) 96
+                        )
+                ),
+                Arguments.of(
+                        "7e0102000c07006195286504fc3037303036313935323836354c7e",
+                        new JT808AuthenticationMessage(
+                                (short) 258,
+                                new BodyProperties(false, false, 0, 12),
+                                "070061952865",
+                                (short) 1276,
+                                new JT808AuthenticationMessage.Body("070061952865"),
+                                (byte) 76
+                        )
+                )
+        );
     }
 }
