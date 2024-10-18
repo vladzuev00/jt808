1b64e872e41da1b7e8dd6674fdb6c61d9c0b52b8
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-18 20:41:43 +0300

    refactor ApplicationRunner

--- src/main/java/by/zuevvlad/jt808/ApplicationRunner.java
+++ src/main/java/by/zuevvlad/jt808/ApplicationRunner.java
@@ -1,6 +1,5 @@
 package by.zuevvlad.jt808;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.springframework.boot.SpringApplication.run;
