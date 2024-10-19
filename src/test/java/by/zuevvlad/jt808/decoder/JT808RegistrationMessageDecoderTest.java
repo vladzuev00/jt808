e5cb3dbd4ec584d0a49b89c5cf63e78aa8528c16
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-19 14:50:00 +0300

    refactor JT808RegistrationMessageDecoderTest

--- src/test/java/by/zuevvlad/jt808/decoder/JT808RegistrationMessageDecoderTest.java
+++ src/test/java/by/zuevvlad/jt808/decoder/JT808RegistrationMessageDecoderTest.java
@@ -1,48 +1,23 @@
-//package by.zuevvlad.jt808.decoder;
-//
-//import by.zuevvlad.jt808.model.JT808RegistrationMessage;
-//import by.zuevvlad.jt808.model.JT808RegistrationMessage.Body;
-//import io.netty.buffer.ByteBuf;
-//import org.junit.jupiter.api.Test;
-//
-//import static io.netty.buffer.ByteBufUtil.decodeHexDump;
-//import static io.netty.buffer.Unpooled.wrappedBuffer;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//public final class JT808RegistrationMessageDecoderTest {
-//    private final JT808RegistrationMessageDecoder decoder = new JT808RegistrationMessageDecoder(null, null);
-//
-//    @Test
-//    public void bodyShouldBeDecoded() {
-//        ByteBuf givenBuffer = wrappedBuffer(decodeHexDump("0000000038363937374E5438303800000000000000000000000000000031393532383635004C42313233343536373839303132333435"));
-//
-//        Body actual = decoder.decodeBody(givenBuffer);
-//        Body expected = new Body((short) 0, (short) 0, "86977", "NT808", "1952865", (byte) 0, "LB123456789012345");
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void messageShouldBeCreated() {
-//        BodyProperties givenBodyProperties = new BodyProperties(true, false, 10, 20);
-//        String givenPhoneNumber = "375446932345";
-//        short givenSerialNumber = 234;
-//        Body givenBody = new Body((short) 0, (short) 0, "86977", "NT808", "1952865", (byte) 0, "LB123456789012345");
-//        byte givenCheckCode = 55;
-//
-//        JT808RegistrationMessage actual = decoder.createMessage(
-//                givenBodyProperties,
-//                givenPhoneNumber,
-//                givenSerialNumber,
-//                givenBody,
-//                givenCheckCode
-//        );
-//        JT808RegistrationMessage expected = new JT808RegistrationMessage(
-//                givenBodyProperties,
-//                givenPhoneNumber,
-//                givenSerialNumber,
-//                givenBody,
-//                givenCheckCode
-//        );
-//        assertEquals(expected, actual);
-//    }
-//}
+package by.zuevvlad.jt808.decoder;
+
+import by.zuevvlad.jt808.model.JT808RegistrationMessage;
+import io.netty.buffer.ByteBuf;
+import org.junit.jupiter.api.Test;
+
+import static io.netty.buffer.ByteBufUtil.decodeHexDump;
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class JT808RegistrationMessageDecoderTest {
+    private final JT808RegistrationMessageDecoder decoder = new JT808RegistrationMessageDecoder();
+
+    @Test
+    public void bufferShouldBeDecodedInternally() {
+        ByteBuf givenBuffer = wrappedBuffer(decodeHexDump("000000003836393737"));
+        String givenPhoneNumber = "375446874433";
+
+        JT808RegistrationMessage actual = decoder.decodeInternal(givenBuffer, givenPhoneNumber);
+        JT808RegistrationMessage expected = new JT808RegistrationMessage(givenPhoneNumber, "86977");
+        assertEquals(expected, actual);
+    }
+}
