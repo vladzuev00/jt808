94e9e29ff508445d35f4fb42dbb13adc4ff1b9ba
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-20 22:32:41 +0300

    add JT808AuthenticationMessageDecoder

---
+++ src/main/java/by/zuevvlad/jt808/decoder/JT808AuthenticationMessageDecoder.java
@@ -0,0 +1,19 @@
+package by.zuevvlad.jt808.decoder;
+
+import by.zuevvlad.jt808.model.JT808AuthenticationMessage;
+import io.netty.buffer.ByteBuf;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class JT808AuthenticationMessageDecoder extends JT808MessageDecoder<JT808AuthenticationMessage> {
+    private static final short MESSAGE_ID = 258;
+
+    public JT808AuthenticationMessageDecoder() {
+        super(MESSAGE_ID);
+    }
+
+    @Override
+    protected JT808AuthenticationMessage decodeInternal(ByteBuf buffer, String phoneNumber) {
+        return new JT808AuthenticationMessage();
+    }
+}
